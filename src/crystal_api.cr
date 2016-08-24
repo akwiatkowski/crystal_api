90e353678bae88f9774299699f3a73b80d532df2
Aleksander Kwiatkowski <bobikx@poczta.fm>
2016-08-24 10:33:44 +0200

    require

--- src/crystal_api.cr
+++ src/crystal_api.cr
@@ -1,6 +1,8 @@
 require "kemal"
 require "kemal-pg"
 
+require "kemal-auth-token/kemal-auth"
+
 require "./crystal_api/version"
 require "./crystal_api/crystal_model"
 require "./crystal_api/crystal_resource"
